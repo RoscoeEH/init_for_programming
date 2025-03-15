d01dd75006de8a9b6048cf9649d854d4cfd7dd82
RoscoeEH <elingshaynier@gmail.com>
2025-03-15 00:41:09 -0700

    Self-install homebrew

--- main.sh
+++ main.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+# Check if Homebrew is installed
+if ! command -v brew &>/dev/null; then
+    echo "Homebrew not found! Installing Homebrew..."
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew is already installed."
+fi
 
 PYTHON_PACKAGES=(
     "python3"
